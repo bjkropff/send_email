499c7cd2affe65279da2a8f423e2e1c91246cbf3
Brian <Brian.Kropff@gmail.com>
2015-08-03 15:27:48 -0700

    Initial commit and cleaning of old code

---
+++ app/app.php
@@ -0,0 +1,39 @@
+<?php
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Task.php";
+
+    use Symfony\Component\Debug\Debug;
+    Debug::enable();
+
+    session_start();
+    if (empty($_SESSION['list_of_tasks'])) {
+        $_SESSION['list_of_tasks'] = array();
+    }
+
+    $app = new Silex\Application();
+    $app['debug'] = true;
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+       'twig.path' => __DIR__.'/../views'
+    ));
+
+    use Symfony\Component\HttpFoundation\Request;
+    Request::enableHttpMethodParameterOverride();
+
+    $app->get("/", function() use ($app) {
+       return $app['twig']->render('task.twig', array('tasks' => Task::getAll()));
+   });
+
+   $app->post("/send", function() use ($app) {
+       $task = new Task($_POST['description']);
+       $task->save();
+       return $app['twig']->render('create_task.twig', array('newtask' => $task));
+   });
+
+   $app->post("/clear", function() use ($app) {
+       Task::deleteAll();
+       return $app['twig']->render('delete_tasks.twig');
+   });
+
+    return $app;
+?>
---
+++ tests/TaskTest.php
@@ -0,0 +1,37 @@
+<?php
+
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+
+    require_once "src/Email.php";
+
+    // $DB = new PDO('pgsql:host=localhost;dbname=to_do_test');
+
+
+    class TaskTest extends PHPUnit_Framework_TestCase
+    {
+
+        protected function tearDown()
+        {
+            Task::deleteAll();
+        }
+
+
+        function test_getDescription()
+        {
+            //Arrange
+
+            $description = "Wash the dog";
+            $test_task = new Task($description);
+            $test_task->save();
+
+            //Act
+            $result = $test_task->getDescription();
+
+            //Assert
+            $this->assertEquals("Wash the dog", $result);
+        }
+    }
+?>
